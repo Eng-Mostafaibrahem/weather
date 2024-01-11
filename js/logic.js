9597d75c4834e644530040118a1e0167e1653135
Eng-Mostafaibrahem <eng.mostafaibrahem99@gmail.com>
2024-01-11 08:23:21 +0200

    second commit

--- js/logic.js
+++ js/logic.js
@@ -1,38 +1,20 @@
-const firstCardTitle = document.querySelector("#firstCardTitle");
-const firstCardText = document.querySelector("#firstCardText");
-const firstCardDate = document.querySelector("#firstCardDate");
-const firstCardDay = document.querySelector("#firstCardDay");
-const cards= document.querySelector("#cards")
-// "firstCardText"
-// "firstCardTitle"
-// "firstCardDate"
-// "firstCardDay"
-console.log(firstCardTitle, "HELLO");
-
-async function getData() {
-  let data = await fetch(
-    "https://api.weatherapi.com/v1/forecast.json?key=4c06365e62eb47a5b05213839240801&q=07112&days=3"
-  );
-
-
-  let res = await data.json();
-  console.log(res.location.name);
-  
+function display(response) {
+  let days = response.forecast.forecastday;
+  let city = response.location.name;
   let cartona = ``;
-  let days = res.forecast.forecastday;
-  
+
   for (let i = 0; i < days.length; i++) {
     const element = days[i];
     console.log(element);
     let date = new Date(element.date);
     //console.log(element.date);
-    let tempreture=element.day.maxtemp_c
-      //console.log(element.day.maxtemp_c);
-    let tempText=element.day.condition.text
+    let tempreture = element.day.maxtemp_c;
+    //console.log(element.day.maxtemp_c);
+    let tempText = element.day.condition.text;
     // console.log(element.day.condition.text);
-    let img = element.day.condition.icon
+    let img = element.day.condition.icon;
     //console.log(element.day.condition.icon);
-    let city=res.location.name
+    //let city=res.location.name
     cartona += `<div class="card border-success mb-3" style="max-width: 18rem">
     <div class="card-header card1-header-bg text-white d-flex justify-content-between ">
         <p class="day">day</p>
@@ -50,10 +32,38 @@ async function getData() {
 
     </div>
   </div>`;
+
+    //searchData(searchelement);
   }
   //console.log(res.forecast.forecastday[0].date
   //);
-  cards.innerHTML=cartona;
+
+  cards.innerHTML = cartona;
+}
+
+async function getData() {
+  let data = await fetch(
+    `https://api.weatherapi.com/v1/forecast.json?key=4c06365e62eb47a5b05213839240801&q=cairo&days=3`
+  );
+
+  let res = await data.json();
+  console.log(res.location.name);
+
+  display(res);
+}
+
+search.addEventListener("blur", function (e) {
+  searchCity(e.target.value);
+});
+
+async function searchCity(element) {
+  let data = await fetch(
+    `https://api.weatherapi.com/v1/forecast.json?key=4c06365e62eb47a5b05213839240801&q=${element}&days=3`
+  );
+
+  let res = await data.json();
+
+  display(res);
 }
 
 getData();
